596e90b50e875e9990cac6365481e600140ae3b2
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-08-22 18:24:02 -0400

    Feature: Add tab navigation

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {Platform, StyleSheet} from 'react-native';
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
+import {createBottomTabNavigator} from 'react-navigation'
+import {FontAwesome, Ionicons} from '@expo/vector-icons'
 import reducer from './reducers'
 import middleware from './middleware'
 import DeckList from "./components/DeckList"
+import NewDeck from "./components/NewDeck"
+
+const white = '#FFFFFF'
+const purple = '#292477'
+
+const Tabs = createBottomTabNavigator({
+    DeckList: {
+        screen: DeckList,
+        navigationOptions: {
+            tabBarLabel: 'List Of Decks',
+            tabBarIcon: ({tintColor}) => <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+        }
+    },
+    AddDeck: {
+        screen: NewDeck,
+        navigationOptions: {
+            tabBarLabel: 'New Deck',
+            tabBarIcon: ({tintColor}) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+        }
+    }
+}, {
+    navigationOptions: {
+        header: null
+    },
+    tabBarOptions: {
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? white : purple,
+            shadowColor: 'rgba(0,0,0,0.24)',
+            shadowOffset: {
+                width: 0,
+                height: 3
+            },
+            shadowRadius: 6,
+            shadowOpacity: 1
+        }
+    }
+})
 
 export default class App extends React.Component {
     render() {
@@ -12,7 +53,7 @@ export default class App extends React.Component {
 
         return (
             <Provider store={store}>
-                <DeckList/>
+                <Tabs/>
             </Provider>
         );
     }
@@ -26,3 +67,4 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
 });
+// TODO: Reconsider icons
