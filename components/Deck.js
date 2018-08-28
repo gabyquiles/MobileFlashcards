0bf2120537e48745badac0f6ee6afd25067899bf
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-08-27 22:13:57 -0400

    Feature: Add animation when selecting a deck

--- components/Deck.js
+++ components/Deck.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {Card, SubTitle, Title} from './Common'
+import {Animated, StyleSheet} from 'react-native'
+import {Card, SubTitle} from './Common'
 
 class Deck extends Component {
+    state = {
+        scaleValue: new Animated.Value(1)
+    }
+
+    animatedAction = () => {
+        const {onPress} = this.props
+        const {scaleValue} = this.state
+        Animated.sequence([
+            Animated.timing(scaleValue, {duration: 200, toValue: 1.5}),
+            Animated.spring(scaleValue, {toValue: 1, friction: 4})
+        ]).start()
+        onPress()
+    }
+
     render() {
         const {deck, onPress} = this.props
+        const {scaleValue} = this.state
         return (
-            <Card onPress={onPress}>
-                <Title>{deck.title}</Title>
-
+            <Card onPress={this.animatedAction}>
+                <Animated.Text style={[styles.title, {transform: [{scale: scaleValue}]}]}>{deck.title}</Animated.Text>
                 <SubTitle>{deck.questions.length} cards</SubTitle>
             </Card>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 30
+    }
+})
 
 function mapStateToProps(decks, {deckId}) {
     return {
