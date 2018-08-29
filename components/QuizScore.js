f3c5865bffc74bfb604ebd71ecd40842184920f4
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-08-28 22:17:28 -0400

    Feature: Add animation in Score Screen

--- components/QuizScore.js
+++ components/QuizScore.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {Platform} from 'react-native'
+import {Animated, Platform} from 'react-native'
 import {NavigationActions, StackActions} from 'react-navigation';
 import {Ionicons} from '@expo/vector-icons'
 import {Button, CenteredColumn, Container, SubTitle, Title} from './Common'
@@ -10,6 +10,17 @@ class QuizScore extends Component {
         title: "Score",
         headerLeft: null
     }
+    state = {
+        scaleValue: new Animated.Value(1)
+    }
+    animateSmiley = () => {
+        const {scaleValue} = this.state
+        Animated.sequence([
+            Animated.timing(scaleValue, {duration: 200, toValue: 1.04}),
+            Animated.spring(scaleValue, {toValue: 1, friction: 4})
+        ]).start()
+    }
+
     toDeck = () => {
         const {navigation} = this.props
         const {deckTitle} = navigation.state.params
@@ -48,20 +59,24 @@ class QuizScore extends Component {
 
     componentDidMount() {
         clearLocalNotification().then(setLocalNotification)
+        this.animateSmiley()
     }
 
     render() {
         const {navigation} = this.props
         const {answeredCorrectly, total, deckTitle} = navigation.state.params
         const correctPercentage = Math.round((answeredCorrectly / total) * 100)
+        const {scaleValue} = this.state
         return (
             <CenteredColumn style={{paddingTop: 10, paddingLeft: 10, paddingRight: 10}}>
                 <Title>Congratulations!!</Title>
                 <Container>
-                    <Ionicons
-                        name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
-                        size={100}
-                    />
+                    <Animated.Text style={{transform: [{scale: scaleValue}]}}>
+                        <Ionicons
+                            name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
+                            size={100}
+                        />
+                    </Animated.Text>
                 </Container>
                 <CenteredColumn>
                     <SubTitle>You answered correctly {correctPercentage}% of the questions</SubTitle>
